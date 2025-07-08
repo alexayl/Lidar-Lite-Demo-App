c7530151485619d1e87804a11d58af8ea91edac4
Alex Aylward <alexaylward@outlook.com>
2025-07-07 20:27:15 -0500

    Working basic driver

--- app/src/main.c
+++ app/src/main.c
@@ -7,26 +7,24 @@ int main(void)
 {
     const struct device *lidar = DEVICE_DT_GET(DT_ALIAS(lidarlite));
     
+    if (!device_is_ready(lidar)) {
+        printk("LIDAR not ready\n");
+        return EXIT_FAILURE;
+    } else {
+        printk("LIDAR is ready\n");
+    }
+
     while (1) {
         if (sensor_sample_fetch(lidar)) {
             printk("sad\n");
         } else {
             printk("happy\n");
+            struct sensor_value val;
+            sensor_channel_get(lidar, SENSOR_CHAN_DISTANCE, &val);
+            printk("Distance: %d cm\n", val.val1);
         }
         k_msleep(500);
     }
-    // if (!device_is_ready(lidar)) {
-    //     printk("LIDAR not ready\n");
-    //     return EXIT_FAILURE;
-    // }
-
-    // while (1) {
-    //     sensor_sample_fetch(lidar);
-    //     struct sensor_value val;
-    //     sensor_channel_get(lidar, SENSOR_CHAN_DISTANCE, &val);
-    //     printk("Distance: %d cm\n", val.val1);
-    //     k_msleep(500);
-    // }
 
     return EXIT_SUCCESS;
 }
--- drivers/sensor/lidar_lite_v4/lidar_lite_v4.c
+++ drivers/sensor/lidar_lite_v4/lidar_lite_v4.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 yo mama
+ * Copyright (c) 2025 Alex
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -13,16 +13,79 @@
 
 int lidar_lite_v4_init(const struct device *dev) {
 
+    const struct lidar_lite_v4_config *config = dev->config;
+    struct lidar_lite_v4_data *data = dev->data;
+
+    if (!device_is_ready(config->i2c.bus)) {
+        return -ENODEV;
+    }
+
     return 0;
 }
 
 static int lidar_lite_v4_sample_fetch(const struct device *dev, enum sensor_channel chan) {
+    struct lidar_lite_v4_data *data = dev->data;
+    const struct lidar_lite_v4_config *config = dev->config;
+    uint8_t status;
+    uint8_t distance_bytes[2];
+    int ret;
+
+    if (!device_is_ready(config->i2c.bus)) {
+        return -ENODEV;
+    }
+
+    // Write command to trigger measurement
+    ret = i2c_reg_write_byte_dt(&config->i2c, LIDAR_LITE_V4_REG_MEASURE, LIDAR_LITE_V4_CMD_MEASURE);
+    if (ret < 0) {
+        return ret;
+    }
+
+    // Read status register and wait until measurement completes
+    int timeout = MAX_TIMEOUT_MS;
+    do {
+        ret = i2c_reg_read_byte_dt(&config->i2c, LIDAR_LITE_V4_REG_STATUS, &status);
+        if (ret < 0) {
+            return ret;
+        }
+        k_msleep(1);
+        timeout--;
+    } while ((status & LIDAR_LITE_V4_STATUS_BUSY) && timeout > 0);
+    if (timeout <= 0) {
+        return -ETIMEDOUT;
+    }
+
+    // Read distance data (high byte then low byte)
+    ret = i2c_reg_read_byte_dt(&config->i2c, LIDAR_LITE_V4_REG_DISTANCE_HIGH, &distance_bytes[0]);
+    if (ret < 0) {
+        return ret;
+    }
     
+    ret = i2c_reg_read_byte_dt(&config->i2c, LIDAR_LITE_V4_REG_DISTANCE_LOW, &distance_bytes[1]);
+    if (ret < 0) {
+        return ret;
+    }
+
+    // Combine high and low bytes to get 16-bit distance in centimeters
+    data->distance = (distance_bytes[0] << 8) | distance_bytes[1];
+
     return 0;
 }
 
-static int lidar_lite_v4_channel_get(const struct device *dev, enum sensor_channel chan, struct sensor_value *val) {
-    
+static int lidar_lite_v4_channel_get(const struct device *dev,
+                    enum sensor_channel chan,
+                    struct sensor_value *val) {
+
+    struct lidar_lite_v4_data *data = dev->data;
+
+    switch (chan) {
+        case SENSOR_CHAN_DISTANCE:
+        case SENSOR_CHAN_ALL:
+            val->val1 = data->distance; // Distance in cm
+            val->val2 = 0; // No fractional part
+            break;
+        default:
+            return -ENOTSUP;
+    }
     return 0;
 }
 
@@ -47,26 +110,3 @@ static const struct sensor_driver_api lidar_lite_v4_driver_api = {
         &lidar_lite_v4_driver_api);
 
 DT_INST_FOREACH_STATUS_OKAY(LIDAR_LITE_V4_DEFINE)
-
-/*
- *
- * @param node_id The devicetree node identifier.
- *
- * @param init_fn Name of the init function of the driver.
- *
- * @param pm_device PM device resources reference (NULL if device does not use
- * PM).
- *
- * @param data_ptr Pointer to the device's private data.
- *
- * @param cfg_ptr The address to the structure containing the configuration
- * information for this instance of the driver.
- *
- * @param level The initialization level. See SYS_INIT() for details.
- *
- * @param prio Priority within the selected initialization level. See
- * SYS_INIT() for details.
- *
- * @param api_ptr Provides an initial pointer to the API function struct used
- * by the driver. Can be NULL.
- */
